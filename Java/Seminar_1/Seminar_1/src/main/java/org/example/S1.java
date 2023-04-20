7e0153c2320654e3c44fc7d4c3759dcfc7740e66
dedani4 <dedani4@gmail.com>
2023-04-20 17:03:28 +0300

    add Homework 1 for Java course

---
+++ Java/Seminar_1/Seminar_1/src/main/java/org/example/S1.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.util.Random;
+
+public class S1 {
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 2000;
+
+        int i = rand(min, max);
+        System.out.printf("Task 1. Random number from 0 to 2000 i = %d\n", i);
+
+        int n = highBit(i);
+        System.out.printf("Task 2. MSB - n = %d\n", n);
+
+        int[] m1 = multiplesNumbers(i, n);
+        System.out.printf("Task 3. Numbers multiples of %d from %d to %d are: ", n, i, Short.MAX_VALUE);
+        for (int j = 0; j<m1.length; j++){
+            if (j == m1.length-1) System.out.printf("%d.\n", m1[j]);
+            else if (j < 3 || j > m1.length-4) System.out.printf("%d ", m1[j]);
+            else if (j == 3) System.out.print(" ... ");
+        }
+
+        int[] m2 = nonMultiplesNumbers(i, n);
+        System.out.printf("Task 4. Numbers non-multiples %d from %d to %d are: ", n, Short.MIN_VALUE, i);
+        for (int j = 0; j<m2.length; j++){
+            if (j == m2.length-1) System.out.printf("%d.\n", m2[j]);
+            else if (j < 3 || j > m2.length-4) System.out.printf("%d ", m2[j]);
+            else if (j == 3) System.out.print(" ... ");
+        }
+
+
+    }
+
+    private static int rand(int min, int max){
+        return new Random().nextInt(min, max);
+    }
+
+    private static int highBit (int i){
+        String binaryI = Integer.toBinaryString(i);
+        return binaryI.length();
+    }
+
+    private  static int[] multiplesNumbers(int i, int n){
+        int[] result = new int[0];
+        for (int j = i; j < Short.MAX_VALUE; j++){
+            if (j % n == 0) result = addElementToArray(j, result);
+        }
+        return result;
+    }
+
+    private  static int[] nonMultiplesNumbers(int i, int n){
+        int[] result = new int[0];
+        for (int j = Short.MIN_VALUE; j < i; j++){
+            if (j % n != 0) result = addElementToArray(j, result);
+        }
+        return result;
+    }
+
+    private static int[] addElementToArray (int element, int[] array){
+        int[] new_array = new int[array.length + 1];
+        System.arraycopy(array, 0, new_array, 0, array.length);
+        new_array[new_array.length-1] = element;
+        return new_array;
+    }
+
+
+}
