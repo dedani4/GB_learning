d45ab10335081c14866bb94a70b361de0850fd90
dedani4 <dedani4@gmail.com>
2023-05-03 11:54:06 +0300

    add Homework 5 for Java course

---
+++ Java/Seminar_1/Seminar_1/src/main/java/org/example/S5_homework.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class S5_homework {
+    public static void main(String[] args) {
+//        Создать множество, ключ и значение строки. Добавить шесть элементов.
+        Map<String,String> map1 = new HashMap<>();
+        map1.put("xxx", "Oleg");
+        map1.put("zzz", "Kolya");
+        map1.put("yyy", "Ivan");
+        map1.put("qqq", "John");
+        map1.put("rrr", "Merlin");
+        map1.put("sss", "Manson");
+//        Вывести в консоль значения.
+        System.out.println(map1.values());
+//        Добавить ко всем значениям символ "!".
+        map1.forEach((k,v) -> map1.compute(k, (k1,v1) -> v1 + "!"));
+        System.out.println(map1.values());
+//        Создать второе множество с таким же обобщением.
+//        Ключи второго множества частично совпадают с ключами первого.
+        Map<String,String> map2 = new HashMap<>();
+        map2.put("aaa", "Huan");
+        map2.put("zzz", "Rashid");
+        map2.put("bbb", "Achiles");
+        map2.put("qqq", "Jesus");
+        map2.put("ccc", "Muhamed");
+        map2.put("sss", "Ali");
+//        Объеденить значания во втором множестве и первом если ключи совподают.
+        map2.putAll(map1);
+//        Вывести результат в консоль.
+        System.out.println(map2);
+
+
+    }
+
+
+}
